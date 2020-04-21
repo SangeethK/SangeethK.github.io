b968d5e59c3191178028506b1733110ce3791075
SangeethK <sangeet.konumuri@gmail.com>
2020-04-21 23:29:11 +0530

    React App

---
+++ precache-manifest.90995d3d116cec30e3ad17de0e31e806.js
@@ -0,0 +1,30 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "494ec341194f0d859097c096677dedcd",
+    "url": "/index.html"
+  },
+  {
+    "revision": "50bad1075f8b39f265d4",
+    "url": "/static/css/main.df34918f.chunk.css"
+  },
+  {
+    "revision": "2624ac88ca346e70f582",
+    "url": "/static/js/2.02bb0411.chunk.js"
+  },
+  {
+    "revision": "e88a3e95b5364d46e95b35ae8c0dc27d",
+    "url": "/static/js/2.02bb0411.chunk.js.LICENSE.txt"
+  },
+  {
+    "revision": "50bad1075f8b39f265d4",
+    "url": "/static/js/main.93537685.chunk.js"
+  },
+  {
+    "revision": "bb5114fc0bd19154b6e0",
+    "url": "/static/js/runtime-main.c992ed73.js"
+  },
+  {
+    "revision": "5d5d9eefa31e5e13a6610d9fa7a283bb",
+    "url": "/static/media/logo.5d5d9eef.svg"
+  }
+]);
